0c582365584391e037397d7ff3b96d1c36bd9d6c
juan2ramos <juan2ramos@gmail.com>
2014-05-13 15:29:36 -0500

    + plantilla inicial

---
+++ layout/default.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <title><?php echo $title ?></title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="author" content="juan2ramos"/>
+    <meta name="contact" content="juan2ramos@gmail.com"/>
+    <meta name="description"content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1"/>
+    <!-- Meta Facebook -->
+    <meta property="og:url" content="http://circulart.com.co/">
+    <meta property="og:title" content="circulart"/>
+    <meta property="og:description" content=""/>
+    <meta property="og:image" content="images/logo.png"/>
+
+    <link rel="shortcut icon" href="favicon.png">
+
+    <!-- Estilos -->
+    <script src="js/prefixfree.min.js"></script>
+    <link rel="stylesheet" href="css/normalize.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+
+
+</head>
+<body>
+<header>
+
+
+</header>
+<nav>
+    <ul>
+        <li><a href="que-es" class="active"></a></li>
+
+    </ul>
+
+
+
+</nav>
+
+<?php include $fileName; ?>
+
+<footer>
+
+</footer>
+<!-- JavaScript -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
